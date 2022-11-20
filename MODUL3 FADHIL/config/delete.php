4f6b5a854e566a1231ba7e4a0ae2c44ba94e0108
fffadhilll <nurmuhammadfadhilah2@gmail.com>
2022-11-20 20:47:20 +0700

    ganti path

--- MODUL3 FADHIL/config/delete.php
+++ MODUL3 FADHIL/config/delete.php
@@ -15,14 +15,14 @@
         echo "
                 <script>
                     alert('Data berhasil dihapus');
-                    document.location.href = '../pages/ListCar-Nama.php';
+                    document.location.href = '../pages/ListCar-Fadhil.php';
                 </script>
             ";
     } else {
         echo "
                 <script>
                     alert('Data tidak berhasil dihapus');
-                    document.location.href = '../pages/ListCar-Nama.php';
+                    document.location.href = '../pages/ListCar-Fadhil.php';
                 </script>
             ";
     }
